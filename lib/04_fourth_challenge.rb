5abb2236c63c418dc21ffa150188776130886d11
Robert Obi <robobi97@googlemail.com>
2020-12-30 20:49:50 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -4,13 +4,14 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [hero_friends_1 = {name: "Benvolio", age: "17", attitude: "worried"} hero_friends_2 = {name: "Mercutio", age: "18", attitude: "hot-headed"}]
+      :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"} {name: "Mercutio", age: "18", attitude: "hot-headed"}
+      ]
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [heroine_friends_1 = {name: "Nurse", age: "44", attitude: "worried"}, heroine_friends_2 = {name: "Steven", age: "30", attitude: "confused"}
+      :heroine_friends => [{name: "Nurse", age: "44", attitude: "worried"}, {name: "Steven", age: "30", attitude: "confused"}
         
       ]
    }
